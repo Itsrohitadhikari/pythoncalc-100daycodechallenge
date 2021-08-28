1d880ae1b6c3c3ae522dbe28033839230f2d3cf0
Rohit Adhikari <89678745+Itsrohitadhikari@users.noreply.github.com>
2021-08-28 14:55:28 +0545

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,26 @@
+import os
+a = input("Enter First Digit = ")
+b = input("Enter Second Digit = ")
+#Converting To Integer:
+a = int(a)
+b = int(b)
+c = a+b
+d = a*b
+e = a-b
+f = b-a
+g = a/b
+h = b/a                                         
+#Adding Two Digits:
+print("The Sum of Entered Numbers Are: ", c)
+#Multiplying Two Digits:
+print("The Multiply Of Entered Digits: ", d)
+#Subtracting Two Digits:-
+print("Subtracting Second Digit With First Digit: ", e)
+print("Subtracting Second Digit With First Digit: ", f)
+#Dividing Digits:
+print("Dividing First Digit with second Digit : ", g)
+print("Dividing Second Digit With First Digit : ", h)
+print()
+print()
+print("Thanks For Using")
+
